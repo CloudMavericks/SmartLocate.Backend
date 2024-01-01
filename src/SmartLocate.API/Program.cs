ce1a3a481f339ff39ec673c7ccc819861788fe6c
SathiyaramanM <Sathiyaraman2003@gmail.com>
2024-01-01 23:27:11 +0530

    Use MMLib.SwaggerForOcelot and setup JWT support in API project

--- src/SmartLocate.API/Program.cs
+++ src/SmartLocate.API/Program.cs
@@ -1,14 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using SmartLocate.Infrastructure.Commons.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddProblemDetails();
 
 builder.Configuration
     .SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+builder.Services.AddJwtAuthentication();
+
 builder.Services.AddOcelot();
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration, 
+    x => x.AddAuthenticationProviderKeyMapping(JwtBearerDefaults.AuthenticationScheme, JwtBearerDefaults.AuthenticationScheme));
 
 var app = builder.Build();
 
@@ -18,6 +31,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseSwaggerForOcelotUI(x =>
+{
+    x.DownstreamSwaggerHeaders = new[]
+    {
+        new KeyValuePair<string, string>("Content-Type", "application/json"),
+        new KeyValuePair<string, string>("Authorization", "Bearer {token}")
+    };
+});
 
 await app.UseOcelot();
 
