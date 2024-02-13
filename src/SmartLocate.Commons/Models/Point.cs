b00952181d0e879736d4de83a3ecc3abb65e0cf5
SathiyaramanM <Sathiyaraman2003@gmail.com>
2024-02-13 23:54:57 +0530

    Update Point model class

--- src/SmartLocate.Commons/Models/Point.cs
+++ src/SmartLocate.Commons/Models/Point.cs
@@ -1,7 +1,31 @@
 namespace SmartLocate.Commons.Models;
 
-public record Point(double Latitude, double Longitude)
+public class Point
 {
+    public Point()
+    {
+        
+    }
+
+    public Point(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+    
+    public Point(double latitude, double longitude, string name)
+    {
+        Name = name;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+    
+    public double Latitude { get; set; }
+    
+    public double Longitude { get; set; }
+    
+    public string Name { get; set; }
+    
     public override string ToString()
     {
         return $"[{Latitude},{Longitude}]";
